def328e08f0456d8b580823a4695b54ff2569268
Bashkim Isai <me@bashkim.com>
2019-03-14 11:37:11 +0000

    chore(sentry): applied sentry to lambda wrapper

--- lambda.js
+++ lambda.js
@@ -1,10 +1,16 @@
+const Raven = require('raven');
+const RavenLambdaWrapper = require('serverless-sentry-lib');
+
 const awsServerlessExpress = require('aws-serverless-express');
 const { app, server } = require('./server');
 
-exports.handler = (event, context) => {
-  app.prepare().then(() => awsServerlessExpress.proxy(
-    awsServerlessExpress.createServer(server),
-    event,
-    context,
-  ));
-};
+exports.handler = RavenLambdaWrapper.handler(
+  Raven,
+  (event, context) => {
+    app.prepare().then(() => awsServerlessExpress.proxy(
+      awsServerlessExpress.createServer(server),
+      event,
+      context,
+    ));
+  },
+);
