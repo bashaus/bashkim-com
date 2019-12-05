e7adee074fc4bf4600298ee576176d12fe69bcb9
Bashkim Isai <me@bashkim.com>
2019-12-05 23:33:03 +0000

    fix(Portfolio): fixed reference to URL

--- components/PortfolioFeaturedCaseStudies/index.jsx
+++ components/PortfolioFeaturedCaseStudies/index.jsx
@@ -22,24 +22,20 @@ export default function PortfolioFeaturedCaseStudies(props) {
         const caseStudy = featuredCaseStudy.data;
 
         return (
-          <li className={styles.item} key={title}>
+          <li className={styles.item} key={feature.featured_case_study.id}>
             <div className={styles.details}>
               { title && RichText.render(title, LinkResolver) }
               { description && RichText.render(description, LinkResolver) }
             </div>
 
             <div className={styles.tile}>
-              <Link href={`/portfolio/${featuredCaseStudy.uid}`}>
-                <a>
-                  <Tile
-                    title={caseStudy.meta_title}
-                    description={caseStudy.meta_description}
-                    href={caseStudy.href}
-                    icon={caseStudy.image_icon.url}
-                    poster={caseStudy.image_poster.url}
-                  />
-                </a>
-              </Link>
+              <Tile
+                title={caseStudy.meta_title}
+                description={caseStudy.meta_description}
+                href={`/portfolio/${featuredCaseStudy.uid}`}
+                icon={caseStudy.image_icon.url}
+                poster={caseStudy.image_poster.url}
+              />
             </div>
           </li>
         );
