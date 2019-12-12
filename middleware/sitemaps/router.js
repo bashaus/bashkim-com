40f1a4136565e8e3bbf005101d15301847e008c1
Bashkim Isai <me@bashkim.com>
2019-12-12 11:36:59 +0000

    chore(robots): moved to public directory

--- middleware/sitemaps/router.js
+++ middleware/sitemaps/router.js
@@ -6,7 +6,6 @@ const Sitemap = require('./generators/sitemap');
 const Config = require('../../config');
 
 const router = express();
-router.use(express.static(path.resolve(__dirname, 'public')));
 router.get(Config.get('sitemap.routes.sitemapIndex.pattern'), SitemapIndex());
 router.get(Config.get('sitemap.routes.sitemap.pattern'), Sitemap());
 
