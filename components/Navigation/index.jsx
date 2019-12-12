a370f597054884d909fe62a58719bd14948ca131
Bashkim Isai <me@bashkim.com>
2019-12-12 15:56:48 +0000

    chore(navigation): social doesn't take theme

--- components/Navigation/index.jsx
+++ components/Navigation/index.jsx
@@ -19,7 +19,7 @@ export default function Navigation(props) {
       `}
     >
       <NavigationPrimary theme={theme} />
-      <NavigationSocial theme={theme} />
+      <NavigationSocial />
     </div>
   );
 }
