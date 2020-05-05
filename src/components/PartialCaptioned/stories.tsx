42735c2e9440cd772443d62bf264667cdb6bf58d
Bashkim Isai <me@bashkim.com>
2020-05-05 21:14:31 +0100

    chore(lint): eslint

--- src/components/PartialCaptioned/stories.tsx
+++ src/components/PartialCaptioned/stories.tsx
@@ -1,16 +1,23 @@
-import React from 'react';
-import { withKnobs, files, text } from '@storybook/addon-knobs';
+import React from "react";
+import { withKnobs, files, text } from "@storybook/addon-knobs";
 
-import PartialCaptioned from '.';
+import PartialCaptioned from ".";
 
 export default {
-  title: 'Partials/Captioned',
+  title: "Partials/Captioned",
   decorators: [withKnobs],
 };
 
 export const Render = (): JSX.Element => (
-  <PartialCaptioned figure={files('figure', '', [])[0]}>
-    <h3>{text('heading', 'heading')}</h3>
-    <p>{text('text', 'text')}</p>
+  <PartialCaptioned
+    figure={
+      <img
+        src={files("figure", "", ["https://placehold.it/1200x630"])[0]}
+        alt=""
+      />
+    }
+  >
+    <h3>{text("heading", "heading")}</h3>
+    <p>{text("text", "text")}</p>
   </PartialCaptioned>
 );
