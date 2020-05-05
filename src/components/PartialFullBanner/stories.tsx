0e013a7e72473c6dd5476b832d3da68eb85ddba8
Bashkim Isai <me@bashkim.com>
2020-05-05 21:10:24 +0100

    chore(styles): aligned headings

--- src/components/PartialFullBanner/stories.tsx
+++ src/components/PartialFullBanner/stories.tsx
@@ -1,20 +1,20 @@
-import React from 'react';
-import { withKnobs, files, text } from '@storybook/addon-knobs';
+import React from "react";
+import { withKnobs, files, text } from "@storybook/addon-knobs";
 
-import PartialFullBanner from '.';
+import PartialFullBanner from ".";
 
 export default {
-  title: 'Partials/Full Banner',
+  title: "Partials/Full Banner",
   decorators: [withKnobs],
 };
 
 export const Render = (): JSX.Element => (
   <PartialFullBanner
-    backgroundDesktop={files('backgroundDesktop', '', [])[0]}
-    backgroundMobile={files('backgroundMobile', '', [])[0]}
+    backgroundDesktop={files("backgroundDesktop", "", [])[0]}
+    backgroundMobile={files("backgroundMobile", "", [])[0]}
     id="PartialFullBanner"
   >
-    <h2>{text('heading', 'heading')}</h2>
-    <p>{text('copy', 'copy')}</p>
+    <h3>{text("heading", "heading")}</h3>
+    <p>{text("copy", "copy")}</p>
   </PartialFullBanner>
 );
