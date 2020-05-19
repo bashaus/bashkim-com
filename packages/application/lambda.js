ebf6f4801e6987e192186ee3193c04c704277921
Bashkim Isai <me@bashkim.com>
2020-05-19 16:31:38 +0100

    chore(next): identify dev

--- packages/application/lambda.js
+++ packages/application/lambda.js
@@ -1,8 +1,9 @@
+const { default: config } = require("@bashkim-com/config");
 const Raven = require("raven");
 const RavenLambdaWrapper = require("serverless-sentry-lib");
 const awsServerlessExpress = require("aws-serverless-express");
 const next = require("next");
-const app = next({ dev: true });
+const app = next(config.next);
 
 module.exports.handler = RavenLambdaWrapper.handler(Raven, (event, context) => {
   app
