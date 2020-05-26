eef9e0aa4e9af5db04cf5e1224e95e4ea6b8b7e9
Bashkim Isai <me@bashkim.com>
2020-05-26 10:47:59 +0100

    feat(cookies): persist cookies consent

--- packages/application/src/contexts/Cookies/index.tsx
+++ packages/application/src/contexts/Cookies/index.tsx
@@ -1,6 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
+import createPersistedState from "use-persisted-state";
+
 import CookiesReducer from "./reducer";
-import { CookiesContextType, INITIAL_STATE } from "./state";
+import {
+  CookiesContextType,
+  CookiesContextState,
+  INITIAL_STATE,
+} from "./state";
+
+const useCounterState = createPersistedState("cookiesDismissed");
 
 export const CookiesContext = React.createContext<CookiesContextType>({
   state: INITIAL_STATE,
@@ -12,7 +20,20 @@ interface CookiesStoreProps {
 }
 
 const CookiesStore = ({ children }: CookiesStoreProps): JSX.Element => {
-  const [state, dispatch] = React.useReducer(CookiesReducer, INITIAL_STATE);
+  const [isDismissed, setIsDismissed] = useCounterState(false);
+
+  /* Retrieve initial state from persisted store */
+  const initialState: CookiesContextState = {
+    ...INITIAL_STATE,
+    isDismissed,
+  };
+
+  const [state, dispatch] = React.useReducer(CookiesReducer, initialState);
+
+  /* Persist state when it changes */
+  useEffect(() => {
+    setIsDismissed(state.isDismissed);
+  }, [state]);
 
   return (
     <CookiesContext.Provider value={{ state, dispatch }}>
