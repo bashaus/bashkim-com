10bbfc35902d39be2a36bd215d71b2d5cdd7f158
Bashkim Isai <me@bashkim.com>
2021-04-24 17:41:14 +0100

    chore(tsconfig): normalise

--- packages/webhooks/src/routes/prismic.ts
+++ packages/webhooks/src/routes/prismic.ts
@@ -3,9 +3,13 @@ import express, { NextFunction, Request, Response } from "express";
 import { triggerCircleCiPipeline } from "../libraries/circleci";
 import { validatePrismicSecret } from "../libraries/prismic";
 
-export type PrismicBody = {
+export type PrismicRequestBody = {
   type: string;
   secret: string;
+};
+
+export type PrismicResponseBody = {
+  ok: boolean;
 };
 
 export const app = express();
@@ -13,8 +17,8 @@ export const app = express();
 app.post(
   "/",
   async (
-    req: Request<unknown, unknown, PrismicBody>,
-    res: Response,
+    req: Request<unknown, PrismicResponseBody, PrismicRequestBody>,
+    res: Response<PrismicResponseBody>,
     next: NextFunction
   ) => {
     const { type, secret } = req.body;
