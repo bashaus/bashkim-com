da15c97139a7de7c91079ab8efdd5b5299d0f957
Bashkim Isai <me@bashkim.com>
2021-04-25 00:32:54 +0100

    chore(prismic): added missing directives

--- packages/prismic/src/index.ts
+++ packages/prismic/src/index.ts
@@ -3,7 +3,10 @@ export * from "./content-types";
 export * from "./slice-types";
 export * from "./slice-types/fixtures";
 
-export { PrismicEmbedType } from "./types/Embed";
-export { PrismicImageType } from "./types/Image";
-export { PrismicLinkType } from "./types/Link";
-export { PrismicSliceType } from "./types/Slice";
+export type { PrismicDateType } from "./types/Date";
+export type { PrismicDateTimeType } from "./types/DateTime";
+export type { PrismicEmbedType } from "./types/Embed";
+export type { PrismicImageType } from "./types/Image";
+export type { PrismicLinkType } from "./types/Link";
+export type { PrismicRelatedDocumentType } from "./types/RelatedDocument";
+export type { PrismicSliceType } from "./types/Slice";
