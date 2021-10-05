2073bb03ab6c845082d79f95a5c67a6e0962a8fc
Bashkim Isai <me@bashkim.com>
2021-10-05 12:00:28 +0100

    chore(next): using <Script /> tag

--- packages/application/src/components/GoogleAnalytics/index.tsx
+++ packages/application/src/components/GoogleAnalytics/index.tsx
@@ -1,38 +1,27 @@
-import { useMemo } from "react";
+import Script from "next/script";
 
 import getConfig from "next/config";
-import Head from "next/head";
 
 export const GoogleAnalytics = (): JSX.Element => {
   const { publicRuntimeConfig: config } = getConfig();
-
-  const GA_SCRIPT = useMemo(
-    () => ({
-      __html: `
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-        gtag('config', '${config.googleAnalytics.ua}');
-      `,
-    }),
-    [config.googleAnalytics.ua]
-  );
 
   if (!config.googleAnalytics.enabled) {
     return null;
   }
 
   return (
-    <Head>
-      <script
-        async
-        key="GoogleAnalytics.external"
+    <>
+      <Script
         src={`https://www.googletagmanager.com/gtag/js?id=${config.googleAnalytics.ua}`}
       />
-      <script
-        key="GoogleAnalytics.internal"
-        dangerouslySetInnerHTML={GA_SCRIPT}
-      />
-    </Head>
+      <Script id="GoogleAnalytics.dataLayer">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${config.googleAnalytics.ua}');
+        `}
+      </Script>
+    </>
   );
 };
