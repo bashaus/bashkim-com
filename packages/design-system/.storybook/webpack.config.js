b743050adef5be913483e03447e4cc22da4ba42e
Bashkim Isai <me@bashkim.com>
2021-10-13 18:11:53 +0100

    chore(webpack): merge

--- packages/design-system/.storybook/webpack.config.js
+++ packages/design-system/.storybook/webpack.config.js
@@ -1,75 +1,57 @@
 const path = require("path");
-const webpack = require("webpack");
+const { merge } = require("webpack-merge");
 
 module.exports = ({ config }) => {
-  config.resolve.alias = {
-    ...config.resolve.alias,
-    "%styleguide": path.resolve(__dirname, "..", "src", "styleguide"),
-  };
-
-  /* svg */
-
   const fileLoaderRule = config.module.rules.find(rule => rule.test.test('.svg'));
   fileLoaderRule.exclude = path.resolve('..')
 
-  config.module.rules.push({
-    test: /\.svg$/,
-    issuer: /\.tsx?$/,
-    use: [
-      { loader: "@svgr/webpack" }
-    ],
-  });
-
-  config.module.rules.push({
-    test: /\.svg$/,
-    issuer: /\.scss$/,
-    type: 'asset/resource',
-  });
-
-  /* css and scss */
-
-  config.module.rules.push({
-    test: /\.scss$/,
-    use: [
-      { loader: "style-loader" },
-      {
-        loader: "css-loader",
-        options: {
-          importLoaders: 1,
-          modules: {
-            localIdentName: "[name]__[local]___[hash:base64:5]",
-          },
-        },
+  return merge(config, {
+    resolve: {
+      alias: {
+        "%styleguide": path.resolve(__dirname, "..", "src", "styleguide"),
       },
-      { loader: "sass-loader" },
-    ],
+      extensions: [".ts", ".tsx"],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.svg$/,
+          issuer: /\.tsx?$/,
+          use: [
+            { loader: "@svgr/webpack" }
+          ],
+        }, {
+          test: /\.svg$/,
+          issuer: /\.scss$/,
+          type: 'asset/resource',
+        }, {
+          test: /\.scss$/,
+          use: [
+            { loader: "style-loader" },
+            {
+              loader: "css-loader",
+              options: {
+                importLoaders: 1,
+                modules: {
+                  localIdentName: "[name]__[local]___[hash:base64:5]",
+                },
+              },
+            },
+            { loader: "sass-loader" },
+          ],
+        }, {
+          test: /\.css$/,
+          use: [
+            { loader: "style-loader" },
+            { loader: "css-loader" },
+          ],
+        }, {
+          test: /\.tsx?$/,
+          use: [
+            { loader: "babel-loader" }
+          ],
+        }
+      ]
+    }
   });
-
-  config.module.rules.push({
-    test: /\.css$/,
-    use: [
-      { loader: "style-loader" },
-      { loader: "css-loader" },
-    ],
-  });
-
-  /* typescript */
-
-  config.resolve.extensions.push(".ts", ".tsx");
-  config.module.rules.push({
-    test: /\.tsx?$/,
-    use: [
-      { loader: "babel-loader" }
-    ],
-  });
-
-  /* jquery */
-  config.plugins.push(
-    new webpack.ProvidePlugin({
-      $: "jquery",
-      jQuery: "jquery",
-    })
-  );
-
-  return config;
 };
