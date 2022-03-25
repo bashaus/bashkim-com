55fd6bfd7557e108f438e6e503a389548587b74f
Bashkim Isai <me@bashkim.com>
2022-03-25 22:03:50 +0000

    chore(deps): adding node-fetch to sitemap to fix serverless jetpack tree shaking

--- packages/sitemap/src/libraries/prismic/client/index.ts
+++ packages/sitemap/src/libraries/prismic/client/index.ts
@@ -1,9 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 import { config } from "@bashkim-com/config";
-import { PrismicLink } from "apollo-link-prismic";
+import { createPrismicLink } from "apollo-link-prismic";
 
 export const PrismicClient = new ApolloClient({
-  link: PrismicLink({
+  link: createPrismicLink({
     uri: config.prismic.uri,
   }),
   cache: new InMemoryCache(),
