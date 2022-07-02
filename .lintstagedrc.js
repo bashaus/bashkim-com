f5f178222a2f4f7a2555dd6a0c1a06fdd80c49a4
Bashkim Isai <me@bashkim.com>
2022-07-02 19:43:19 +0100

    feat: implement svgo for cleaning svg files

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -2,8 +2,5 @@ module.exports = {
   "package.json": ["sort-package-json"],
   "!(styles).{ts,tsx}": ["eslint --fix"],
   "styles.ts": ["eslint --fix", "stylelint --fix"],
-  "*.{svg,xml}": (filenames) =>
-    filenames.map(
-      (filename) => `xmllint --format ${filename} --output ${filename}`
-    ),
+  "*.svg": ["svgo"],
 };
