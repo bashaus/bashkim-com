38695dad550def8c7d09abfa3296fddb503fd21b
Bashkim Isai <me@bashkim.com>
2022-08-14 12:14:06 +0100

    chore(application): change copyright date

--- packages/application/src/components/FooterCopyright/index.tsx
+++ packages/application/src/components/FooterCopyright/index.tsx
@@ -3,7 +3,7 @@ import * as S from "./styles";
 export const FooterCopyright = () => (
   <S.FooterCopyright
     role="contentinfo"
-    aria-label="Copyright, Two-Thousand-and-Eight to Two-Thousand-and-Twenty, Bashkim Isai. All rights reserved."
+    aria-label="Copyright, Two-Thousand-and-Eight to Two-Thousand-and-Twenty-Two, Bashkim Isai. All rights reserved."
   >
     <small aria-hidden="true">
       Copyright &copy;{" "}
@@ -11,8 +11,8 @@ export const FooterCopyright = () => (
         MMVIII
       </time>
       &#x2011;
-      <time dateTime="2021" title="2021">
-        MMXXI
+      <time dateTime="2022" title="2022">
+        MMXXII
       </time>
       , Bashkim&nbsp;Isai. All&nbsp;rights&nbsp;reserved.
     </small>
