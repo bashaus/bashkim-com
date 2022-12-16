b09af37deef7b9781fb25be08a84358b704e9a63
Bashkim Isai <me@bashkim.com>
2022-12-16 15:00:21 +0000

    chore(terraform): migrate to workspaces

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,8 +1,9 @@
 terraform {
   backend "remote" {
     organization = "bashaus"
+
     workspaces {
-      name = "bashkim-com"
+      prefix = "bashkim-com-"
     }
   }
 
@@ -16,22 +17,22 @@ terraform {
 }
 
 provider "aws" {
-  region  = "eu-west-1"
+  region = "eu-west-1"
 }
 
 module "design-system" {
   source = "./modules/design-system"
 
-  stack_name = "bashkim-com"
+  stack_name      = "bashkim-com"
   certificate_arn = "arn:aws:acm:us-east-1:409535992503:certificate/931d3c5c-33c6-410e-a01f-f45176fb0b99"
-  domain_name = "design-system.bashkim.com"
+  domain_name     = "design-system.bashkim.com"
 }
 
 module "website" {
   source = "./modules/website"
 
-  stack_name = "bashkim-com"
-  certificate_arn = "arn:aws:acm:us-east-1:409535992503:certificate/931d3c5c-33c6-410e-a01f-f45176fb0b99"
-  domain_name = "www.bashkim.com"
+  stack_name            = "bashkim-com"
+  certificate_arn       = "arn:aws:acm:us-east-1:409535992503:certificate/931d3c5c-33c6-410e-a01f-f45176fb0b99"
+  domain_name           = "www.bashkim.com"
   serverless_api_domain = "j1rzctrvgj.execute-api.eu-west-1.amazonaws.com"
 }
