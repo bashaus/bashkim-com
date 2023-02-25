b6d5eb7ef5b51ebb385bbde6bddb973e781b9797
Bashkim Isai <me@bashkim.com>
2023-02-25 23:26:22 +0000

    chore(website): remove style-components from next configuration

--- apps/website/next.config.js
+++ apps/website/next.config.js
@@ -7,10 +7,6 @@ const { withNx } = require("@nrwl/next/plugins/with-nx");
  * @type {import('@nrwl/next/plugins/with-nx').WithNxOptions}
  **/
 const nextConfig = {
-  compiler: {
-    styledComponents: true,
-  },
-
   trailingSlash: true,
 
   nx: {
