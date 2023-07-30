e3bdabbb139b1e3d83e01b119c2834344d384d25
Bashkim Isai <me@bashkim.com>
2023-07-30 09:23:29 +0100

    chore(deps-dev): bump storybook from 7.1.0 to 7.1.1

--- packages/design-system/.storybook/main.ts
+++ packages/design-system/.storybook/main.ts
@@ -1,18 +1,14 @@
 import type { StorybookConfig } from "@storybook/types";
-
 const config: StorybookConfig = {
   addons: ["@storybook/addon-a11y", "@storybook/addon-essentials"],
-
   core: {
     disableTelemetry: true,
     enableCrashReports: false,
   },
-
   framework: {
     name: "@storybook/nextjs",
     options: {},
   },
-
   stories: [
     "../src/components/**/*.stories.tsx",
     "../src/formatters/**/*.stories.tsx",
@@ -20,6 +16,8 @@ const config: StorybookConfig = {
     "../src/partials/**/*.stories.tsx",
     "../src/slices/**/*.stories.tsx",
   ],
+  docs: {
+    autodocs: true,
+  },
 };
-
 export default config;
