a03b04ba3934218a0e66b5e45ef910efff7996d6
Bashkim Isai <me@bashkim.com>
2023-07-30 12:02:26 +0100

    fix(socials-server): using a different api gateway handler

--- packages/socials-server/src/lambda.ts
+++ packages/socials-server/src/lambda.ts
@@ -9,5 +9,5 @@ import { app } from "./app";
 
 export const handler = startServerAndCreateLambdaHandler(
   app,
-  handlers.createAPIGatewayProxyEventV2RequestHandler(),
+  handlers.createAPIGatewayProxyEventRequestHandler(),
 );
