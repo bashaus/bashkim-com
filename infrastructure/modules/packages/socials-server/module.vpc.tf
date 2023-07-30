fe5f40076bedf78a48e7631e3e89543a4fd6eee7
Bashkim Isai <me@bashkim.com>
2023-07-31 00:20:14 +0100

    ci(infrastructure): enable dns support

--- infrastructure/modules/packages/socials-server/module.vpc.tf
+++ infrastructure/modules/packages/socials-server/module.vpc.tf
@@ -1,5 +1,7 @@
 resource "aws_vpc" "elasticache_vpc" {
   cidr_block = "10.0.0.0/16"
+  enable_dns_support = true
+  enable_dns_hostnames = true
 
   tags = {
     Name = "${var.stack_name}-elasticache-vpc"
