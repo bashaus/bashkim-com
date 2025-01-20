92eb31fd7221bef5086b9a069a956756a504812a
Bashkim Isai <me@bashkim.com>
2025-01-20 21:54:03 +0100

    fix: social icon color

--- packages/website/src/domains/socials-linked-in/SocialLinkedInHeader/index.tsx
+++ packages/website/src/domains/socials-linked-in/SocialLinkedInHeader/index.tsx
@@ -8,7 +8,7 @@ export default function SocialLinkedInHeader() {
     <SocialHeader
       href="https://www.linkedin.com/in/bashaus/"
       avatarBadgeBackground="#0077B5"
-      avatarBadge={<LinkedInVector width={10} height={10} color="#FFFFF" />}
+      avatarBadge={<LinkedInVector width={10} height={10} color="#FFFFFF" />}
       textPrimary="Bashkim Isai"
       textSecondary="View LinkedIn profile"
     />
--- packages/website/src/domains/socials-medium/SocialMediumHeader/index.tsx
+++ packages/website/src/domains/socials-medium/SocialMediumHeader/index.tsx
@@ -6,7 +6,7 @@ export default function SocialMediumHeader() {
     <SocialHeader
       href="https://medium.com/@bashaus"
       avatarBadgeBackground="#000"
-      avatarBadge={<MediumVector width={10} height={10} color="#FFFFF" />}
+      avatarBadge={<MediumVector width={10} height={10} color="#FFFFFF" />}
       textPrimary="Bashkim Isai"
       textSecondary="View profile on Medium"
     />
