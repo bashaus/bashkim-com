d9a90824c8059d5a0f54adf4b9bba4406e0af59a
Bashkim Isai <me@bashkim.com>
2025-02-17 10:34:31 +0100

    chore: storybook 8.5.6

--- packages/design-system/.storybook/main.ts
+++ packages/design-system/.storybook/main.ts
@@ -1,13 +1,21 @@
 import type { StorybookConfig } from "@storybook/nextjs";
+import { dirname, join } from "path";
 import TsconfigPathsPlugin from "tsconfig-paths-webpack-plugin";
 import merge from "webpack-merge";
 
+function getAbsolutePath(value: string): string {
+  return dirname(require.resolve(join(value, "package.json")));
+}
+
 const config: StorybookConfig = {
   stories: ["../src/**/stories.@(ts|tsx)"],
-  addons: ["@storybook/addon-essentials", "@storybook/addon-a11y"],
+  addons: [
+    getAbsolutePath("@storybook/addon-essentials"),
+    getAbsolutePath("@storybook/addon-a11y"),
+  ],
 
   framework: {
-    name: "@storybook/nextjs",
+    name: getAbsolutePath("@storybook/nextjs"),
     options: {},
   },
 
