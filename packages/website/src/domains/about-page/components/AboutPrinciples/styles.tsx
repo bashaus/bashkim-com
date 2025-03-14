a7f52af243756ef9fceccd7a8d61c0f72e230c18
Bashkim Isai <me@bashkim.com>
2025-03-14 21:31:46 +0100

    chore: readonly props

--- packages/website/src/domains/about-page/components/AboutPrinciples/styles.tsx
+++ packages/website/src/domains/about-page/components/AboutPrinciples/styles.tsx
@@ -1,7 +1,7 @@
 import Grid, { Grid2Props as GridProps } from "@mui/material/Grid2";
 import { styled } from "@mui/system";
 
-type GridCellProps = Omit<GridProps, "size">;
+type GridCellProps = Readonly<Omit<GridProps, "size">>;
 
 function GridCell(props: GridCellProps) {
   return <Grid {...props} size={{ xs: 12, sm: 6, md: 3, lg: 3, xl: 3 }} />;
