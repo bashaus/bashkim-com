145be7c49faa7f4863f93770bc8ad1cef770594d
Bashkim Isai <me@bashkim.com>
2025-03-26 19:38:52 +0100

    chore: update max-width

--- packages/design-system/src/partials/FullTextPartial/index.tsx
+++ packages/design-system/src/partials/FullTextPartial/index.tsx
@@ -7,16 +7,7 @@ export type FullTextPartialProps = Readonly<PropsWithChildren>;
 
 export default function FullTextPartial({ children }: FullTextPartialProps) {
   return (
-    <Container
-      sx={(theme) => ({
-        [theme.breakpoints.down("md")]: {
-          maxWidth: 526,
-        },
-        [theme.breakpoints.up("md")]: {
-          maxWidth: 648,
-        },
-      })}
-    >
+    <Container maxWidth="md">
       <RichTextFormatter>{children}</RichTextFormatter>
     </Container>
   );
