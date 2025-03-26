61686e40293e33d5954d96da2ae5ec1b908952c8
Bashkim Isai <me@bashkim.com>
2025-03-26 19:43:17 +0100

    fix: set cookies accepted returns no value

--- packages/website/src/domains/cookies-page/actions/set-cookies-accepted.ts
+++ packages/website/src/domains/cookies-page/actions/set-cookies-accepted.ts
@@ -4,5 +4,5 @@ import { cookies } from "next/headers";
 
 export async function setCookiesAccepted() {
   const cookieJar = await cookies();
-  return cookieJar.set("cookies", "accepted");
+  cookieJar.set("cookies", "accepted");
 }
