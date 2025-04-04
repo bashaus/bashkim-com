b13f613ac932d9d9d6319c9d5fe5e45f8ae5f209
Bashkim Isai <me@bashkim.com>
2025-04-04 11:21:10 +0200

    refactor: cookies page mui

--- packages/website/src/domains/cookies-page/components/CookiesDetails/index.tsx
+++ packages/website/src/domains/cookies-page/components/CookiesDetails/index.tsx
@@ -2,11 +2,17 @@ import FullTextPartial from "@bashkim-com/design-system/FullTextPartial";
 import Group from "@bashkim-com/design-system/Group";
 import PageHeaderPartial from "@bashkim-com/design-system/PageHeaderPartial";
 import Divider from "@mui/material/Divider";
+import Link from "@mui/material/Link";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Typography from "@mui/material/Typography";
 
 import HeaderDesktopImage from "@/domains/cookies-page/assets/header-desktop.jpg";
 import HeaderMobileImage from "@/domains/cookies-page/assets/header-mobile.jpg";
-
-import styles from "./styles.module.scss";
 
 export default function CookiesDetails() {
   return (
@@ -18,41 +24,51 @@ export default function CookiesDetails() {
 
       <Group>
         <FullTextPartial>
-          <h1>Cookie policy</h1>
-          <p>
+          <Typography variant="h5" component="h1" gutterBottom>
+            Cookies on this site
+          </Typography>
+
+          <Typography gutterBottom>
             This website sends and receives data (known as <em>cookies</em>) to
             understand how you browse the site and deliver the best possible
             experience. These cookies measure how you use the website to inform
             improvements with insights.
-          </p>
+          </Typography>
 
-          <p>
+          <Typography gutterBottom>
             No personally identifiable information is collected through the use
             of cookies on this site.
-          </p>
+          </Typography>
 
-          <p>
+          <Typography gutterBottom>
             More information about{" "}
-            <a
+            <Link
               href="https://ico.org.uk/for-the-public/online/cookies/"
               target="_blank"
               rel="noreferrer"
             >
               how to manage cookies
-            </a>{" "}
-            can be found on the Information&nbsp;Commissioner&apos;s&nbsp;Office
+            </Link>{" "}
+            can be found on the Information Commissioner&apos;s Office (ICO)
             website.
-          </p>
+          </Typography>
 
-          <h2>How are cookies used on this website?</h2>
+          <Typography variant="h5" component="h2" gutterBottom>
+            How are cookies used on this website?
+          </Typography>
 
-          <h3>Google Analytics</h3>
-          <p>
+          <Typography variant="h6" component="h3" gutterBottom>
+            Google Analytics
+          </Typography>
+
+          <Typography gutterBottom>
             Google Analytics collects information about how you engage with this
             website; this informs decisions on making improvements for the site.
-          </p>
+          </Typography>
 
-          <p>Google Analytics stores information about:</p>
+          <Typography gutterBottom>
+            Google Analytics stores information about:
+          </Typography>
 
           <ul>
             <li>The pages you visit on this website;</li>
@@ -61,78 +77,82 @@ export default function CookiesDetails() {
             <li>What you click on during your visit to this website.</li>
           </ul>
 
-          <p>
+          <Typography gutterBottom>
             This information is collected anonymously and no personally
             identifiable information (e.g.: your name or address) is collected
             or stored.
-          </p>
+          </Typography>
 
-          <p>Google Analytics sets the following cookies:</p>
+          <Typography gutterBottom>
+            Google Analytics sets the following cookies:
+          </Typography>
 
-          <table className={styles["CookiesTable"]}>
-            <thead>
-              <tr>
-                <th>Name</th>
-                <th>Purpose</th>
-                <th>Lifetime</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <th>_ga</th>
-                <td>
-                  Helps count how many people visit this website by tracking if
-                  you&apos;ve visited before.
-                </td>
-                <td>2 years</td>
-              </tr>
-              <tr>
-                <th>_gid</th>
-                <td>
-                  Helps count how many people visit this website by tracking if
-                  you&apos;ve visited before.
-                </td>
-                <td>24 hours</td>
-              </tr>
-              <tr>
-                <th>_gat</th>
-                <td>
-                  Used to manage the rate at which page view requests are made.
-                </td>
-                <td>10 minutes</td>
-              </tr>
-            </tbody>
-          </table>
+          <TableContainer>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell>Name</TableCell>
+                  <TableCell>Purpose</TableCell>
+                  <TableCell>Lifetime</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                <TableRow>
+                  <TableCell>_ga</TableCell>
+                  <TableCell>
+                    Helps count how many people visit this website by tracking
+                    if you&apos;ve visited before.
+                  </TableCell>
+                  <TableCell>2 years</TableCell>
+                </TableRow>
 
-          <p>
-            The implementation of Google Analytics has been requested to
-            anonymise your IP address to help protect your identity.
-          </p>
+                <TableRow>
+                  <TableCell>_gid</TableCell>
+                  <TableCell>
+                    Helps count how many people visit this website by tracking
+                    if you&apos;ve visited before.
+                  </TableCell>
+                  <TableCell>24 hours</TableCell>
+                </TableRow>
 
-          <p>
+                <TableRow>
+                  <TableCell>_gat</TableCell>
+                  <TableCell>
+                    Used to manage the rate at which page view requests are
+                    made.
+                  </TableCell>
+                  <TableCell>10 minutes</TableCell>
+                </TableRow>
+              </TableBody>
+            </Table>
+          </TableContainer>
+
+          <Typography gutterBottom>
             If you prefer, you can{" "}
-            <a
+            <Link
               href="https://tools.google.com/dlpage/gaoptout"
               target="_blank"
               rel="noreferrer"
             >
               opt-out of Google Analytics
-            </a>{" "}
+            </Link>{" "}
             by following the instructions provided by Google.
-          </p>
+          </Typography>
 
-          <h3>YouTube</h3>
+          <Typography variant="h6" component="h3" gutterBottom>
+            YouTube
+          </Typography>
 
-          <p>
+          <Typography gutterBottom>
             This website utilises the embedded player from YouTube to present
             videos of work and associated artefacts.
-          </p>
+          </Typography>
 
           <Divider />
 
-          <p>
-            <small>Last updated 18 July 2022</small>
-          </p>
+          <Typography gutterBottom fontSize="small">
+            Last updated 30 March 2025
+          </Typography>
         </FullTextPartial>
       </Group>
     </>
