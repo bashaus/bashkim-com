155690500c41753a39559935d46c436d07e8f6b9
Bashkim Isai <me@bashkim.com>
2025-04-04 10:29:04 +0200

    chore: disable cookie toast

--- packages/website/src/layouts/SiteLayout/index.tsx
+++ packages/website/src/layouts/SiteLayout/index.tsx
@@ -10,7 +10,6 @@ import { type PropsWithChildren } from "react";
 import GridLines from "@/components/GridLines";
 import ToastController from "@/components/ToastController";
 import NavigationProvider from "@/contexts/Navigation/provider";
-import { useCookiesToast } from "@/domains/cookies-page/hooks/use-cookies-toast";
 import SiteSettingsProvider from "@/domains/site-settings/contexts/SiteSettings/provider";
 import theme from "@/libraries/material-ui/theme";
 import PrismicLinkResolver from "@/libraries/prismic/PrismicLinkResolver";
@@ -18,8 +17,6 @@ import PrismicLinkResolver from "@/libraries/prismic/PrismicLinkResolver";
 export type SiteLayoutProps = Readonly<PropsWithChildren>;
 
 export default function SiteLayout({ children }: SiteLayoutProps) {
-  useCookiesToast();
-
   return (
     <SiteSettingsProvider>
       <ThemeProvider theme={theme}>
