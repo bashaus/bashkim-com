8c2475cd67acdf1be4b0e32c06c3c4e65dffb66b
Bashkim Isai <me@bashkim.com>
2025-07-12 15:34:47 +0200

    chore: bump storybook from 8.6.12 to 9.0.16

--- packages/design-system/.storybook/main.ts
+++ packages/design-system/.storybook/main.ts
@@ -10,8 +10,8 @@ function getAbsolutePath(value: string): string {
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/stories.@(ts|tsx)"],
   addons: [
-    getAbsolutePath("@storybook/addon-essentials"),
     getAbsolutePath("@storybook/addon-a11y"),
+    getAbsolutePath("@storybook/addon-docs"),
   ],
 
   framework: {
--- packages/design-system/eslint.config.mjs
+++ packages/design-system/eslint.config.mjs
@@ -1,7 +1,9 @@
 import eslintConfig from "@bashkim-com/eslint";
+import storybook from "eslint-plugin-storybook";
 
 const config = [
   ...eslintConfig.configs.react,
+  ...storybook.configs["flat/recommended"],
   {
     ignores: ["storybook-static/*"],
   },
