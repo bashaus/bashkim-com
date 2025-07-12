eed799e52b0348bd695f150595cdbb80625d6fb0
Bashkim Isai <me@bashkim.com>
2025-07-12 23:23:53 +0200

    refactor: grid embed slice

--- packages/design-system/src/slices/GridEmbedSlice/index.tsx
+++ packages/design-system/src/slices/GridEmbedSlice/index.tsx
@@ -2,8 +2,6 @@ import type { GridEmbedSliceTypeFragment } from "@bashkim-com/prismic-dal";
 import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
 
-import styles from "./styles.module.scss";
-
 export type GridEmbedSliceProps = Readonly<{
   slice: GridEmbedSliceTypeFragment;
 }>;
@@ -12,16 +10,11 @@ export default function GridEmbedSlice({ slice }: GridEmbedSliceProps) {
   return (
     <Container>
       <Grid container rowSpacing={1} columnSpacing={2}>
-        {slice.fields?.map((field) => {
+        {slice.fields?.map((field, i) => {
           const { grid_embed_slice_type_embed: embed } = field;
           return (
-            <Grid
-              size={{ xs: 12, sm: 6, md: 6, lg: 4, xl: 4 }}
-              key={JSON.stringify(field)}
-            >
-              <div className={styles["Embed"]} key={JSON.stringify(field)}>
-                <div dangerouslySetInnerHTML={{ __html: embed?.html ?? "" }} />
-              </div>
+            <Grid size={{ xs: 12, sm: 6, md: 6, lg: 4, xl: 4 }} key={i}>
+              <div dangerouslySetInnerHTML={{ __html: embed?.html ?? "" }} />
             </Grid>
           );
         })}
