b28c4c3285a708de2617e368d6bf27282692bac8
Bashkim Isai <me@bashkim.com>
2025-07-13 10:56:22 +0200

    test: story from vimeo

--- packages/design-system/src/components/VideoBackground/stories.tsx
+++ packages/design-system/src/components/VideoBackground/stories.tsx
@@ -5,12 +5,13 @@ import VideoBackground from ".";
 export default {
   component: VideoBackground,
   title: "Components/Video Background",
-  args: {
-    url: "https://vimeo.com/1067276219",
-  },
   parameters: {
     layout: "fullscreen",
   },
 } as Meta;
 
-export const Fixture = {};
+export const Vimeo = {
+  args: {
+    url: "https://vimeo.com/1067276219",
+  },
+} as Meta;
