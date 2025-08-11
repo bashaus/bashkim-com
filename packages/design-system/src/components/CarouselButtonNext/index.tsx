ec5543a5b8371bcb7018fa20236108e64aa366bf
Bashkim Isai <me@bashkim.com>
2025-08-12 00:24:35 +0200

    fix: sonarqube

--- packages/design-system/src/components/CarouselButtonNext/index.tsx
+++ packages/design-system/src/components/CarouselButtonNext/index.tsx
@@ -2,7 +2,9 @@ import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 
 import CarouselButton, { CarouselButtonProps } from "../CarouselButton";
 
-export type CarouselButtonNextProps = Omit<CarouselButtonProps, "children">;
+export type CarouselButtonNextProps = Readonly<
+  Omit<CarouselButtonProps, "children">
+>;
 
 export default function CarouselButtonNext(props: CarouselButtonNextProps) {
   return (
