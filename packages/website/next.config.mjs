b4a5070bea2ca3bca7e95b6651bd7ac88766ca23
Bashkim Isai <me@bashkim.com>
2025-08-22 19:45:09 +0200

    chore: http headers

--- packages/website/next.config.mjs
+++ packages/website/next.config.mjs
@@ -13,6 +13,44 @@ const nextConfig = {
     unoptimized: true,
   },
 
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          /**
+           * @see https://developer.mozilla.org/en-US/docs/Web/HTTP/Reference/Headers/X-Content-Type-Options
+           */
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+
+          /**
+           * @see https://developer.mozilla.org/en-US/docs/Web/HTTP/Reference/Headers/X-DNS-Prefetch-Control
+           */
+          {
+            key: "X-DNS-Prefetch-Control",
+            value: "on",
+          },
+
+          /**
+           * @see https://developer.mozilla.org/en-US/docs/Web/HTTP/Reference/Headers/Referrer-Policy
+           */
+          { key: "Referrer-Policy", value: "strict-origin-when-cross-origin" },
+
+          /**
+           * @see https://developer.mozilla.org/en-US/docs/Web/HTTP/Reference/Headers/Permissions-Policy
+           */
+          {
+            key: "Permissions-Policy",
+            value: "camera=(), geolocation=(), microphone=(), payment=()",
+          },
+        ],
+      },
+    ];
+  },
+
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
