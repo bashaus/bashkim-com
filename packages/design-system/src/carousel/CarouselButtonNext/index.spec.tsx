4dc00ce7779d46ecdba6978a10f0b2c8dab9a812
Bashkim Isai <me@bashkim.com>
2026-03-28 20:14:50 +0100

    chore: do not mix storybook with vitest

--- packages/design-system/src/carousel/CarouselButtonNext/index.spec.tsx
+++ packages/design-system/src/carousel/CarouselButtonNext/index.spec.tsx
@@ -1,10 +1,6 @@
-import { composeStories } from "@storybook/nextjs";
 import { fireEvent, render } from "@testing-library/react";
 
 import CarouselButtonNext from ".";
-import * as stories from "./stories";
-
-const SB = composeStories(stories);
 
 describe("<CarouselButtonNext />", () => {
   it("should handle click", () => {
@@ -15,19 +11,5 @@ describe("<CarouselButtonNext />", () => {
     fireEvent.click(button);
 
     expect(onClick).toHaveBeenCalledTimes(1);
-  });
-
-  describe("stories", () => {
-    it("#enabled", () => {
-      const { getByLabelText } = render(<SB.Enabled />);
-      const button = getByLabelText("Next");
-      expect(button).toBeInTheDocument();
-    });
-
-    it("#disabled", () => {
-      const { getByLabelText } = render(<SB.Disabled />);
-      const button = getByLabelText("Next");
-      expect(button).toBeInTheDocument();
-    });
   });
 });
