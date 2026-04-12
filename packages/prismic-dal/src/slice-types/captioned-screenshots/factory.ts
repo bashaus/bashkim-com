1ea007c013ac9bcdb5128f20ab409729ae0b3c72
Bashkim Isai <me@bashkim.com>
2026-04-12 14:10:20 +0200

    chore: simplify screenshots factory

--- packages/prismic-dal/src/slice-types/captioned-screenshots/factory.ts
+++ packages/prismic-dal/src/slice-types/captioned-screenshots/factory.ts
@@ -10,77 +10,58 @@ import { prismicHeading3Factory } from "../../prismic/heading/factory";
 import { prismicParagraphFactory } from "../../prismic/paragraph/factory";
 
 export const captionedScreenshotsSliceFieldFactory =
-  Factory.define<CaptionedScreenshotsSliceTypeFieldFragment>(({ params }) => {
-    const { width = 320, height = 1362 } =
-      params.captioned_screenshots_slice_type_images?.dimensions ?? {};
-
-    return {
-      __typename: "Case_studyBodyCaptionedscreenshotsslicetypeFields",
-      captioned_screenshots_slice_type_images: prismicImageFactory.build({
-        dimensions: { width, height },
-      }),
-    };
-  });
+  Factory.define<CaptionedScreenshotsSliceTypeFieldFragment>(
+    ({ associations }) => {
+      return {
+        __typename: "Case_studyBodyCaptionedscreenshotsslicetypeFields",
+        captioned_screenshots_slice_type_images:
+          associations.captioned_screenshots_slice_type_images ??
+          prismicImageFactory.build({
+            dimensions: { width: 320, height: 1362 },
+          }),
+      };
+    },
+  );
 
 export const captionedScreenshotsSlicePrimaryFactory =
-  Factory.define<CaptionedScreenshotsSliceTypePrimaryFragment>(() => {
-    return {
-      __typename: "Case_studyBodyCaptionedscreenshotsslicetypePrimary",
-      captioned_screenshots_slice_type_caption: [
-        prismicHeading3Factory.build(),
-        prismicParagraphFactory.build(),
-      ],
-    };
-  });
+  Factory.define<CaptionedScreenshotsSliceTypePrimaryFragment>(
+    ({ associations }) => {
+      return {
+        __typename: "Case_studyBodyCaptionedscreenshotsslicetypePrimary",
+        captioned_screenshots_slice_type_caption:
+          associations.captioned_screenshots_slice_type_caption ?? [
+            prismicHeading3Factory.build(),
+            prismicParagraphFactory.build(),
+          ],
+      };
+    },
+  );
 
 export const captionedScreenshotsSliceFactory =
-  Factory.define<CaptionedScreenshotsSliceTypeFragment>(() => {
+  Factory.define<CaptionedScreenshotsSliceTypeFragment>(({ associations }) => {
     return {
       __typename: "Case_studyBodyCaptionedscreenshotsslicetype",
       type: "CaptionedScreenshotsSliceType",
-      primary: captionedScreenshotsSlicePrimaryFactory.build(),
-      fields: [
-        captionedScreenshotsSliceFieldFactory.build({
-          captioned_screenshots_slice_type_images: {
-            dimensions: { width: 320, height: 1362 },
-            alt: "Mobile Portrait",
-          },
-        }),
-
-        captionedScreenshotsSliceFieldFactory.build({
-          captioned_screenshots_slice_type_images: {
-            dimensions: { width: 480, height: 1160 },
-            alt: "Mobile Landscape",
-          },
-        }),
-
-        captionedScreenshotsSliceFieldFactory.build({
-          captioned_screenshots_slice_type_images: {
-            dimensions: { width: 768, height: 1135 },
-            alt: "Tablet Portrait",
-          },
-        }),
-
-        captionedScreenshotsSliceFieldFactory.build({
-          captioned_screenshots_slice_type_images: {
-            dimensions: { width: 1024, height: 650 },
-            alt: "Tablet Landscape",
-          },
-        }),
-
-        captionedScreenshotsSliceFieldFactory.build({
-          captioned_screenshots_slice_type_images: {
-            dimensions: { width: 1280, height: 800 },
-            alt: "Desktop Narrow",
-          },
-        }),
-
-        captionedScreenshotsSliceFieldFactory.build({
-          captioned_screenshots_slice_type_images: {
-            dimensions: { width: 1440, height: 800 },
-            alt: "Desktop Wide",
-          },
-        }),
-      ],
+      primary:
+        associations.primary ?? captionedScreenshotsSlicePrimaryFactory.build(),
+      fields:
+        associations.fields ??
+        Object.entries(screenSizes).map(([alt, dimensions]) =>
+          captionedScreenshotsSliceFieldFactory.build({
+            captioned_screenshots_slice_type_images: prismicImageFactory.build({
+              alt,
+              dimensions,
+            }),
+          }),
+        ),
     };
   });
+
+const screenSizes = {
+  "Mobile Portrait": { width: 320, height: 1362 },
+  "Mobile Landscape": { width: 480, height: 1160 },
+  "Tablet Portrait": { width: 768, height: 1135 },
+  "Tablet Landscape": { width: 1024, height: 650 },
+  "Desktop Narrow": { width: 1280, height: 800 },
+  "Desktop Wide": { width: 1440, height: 800 },
+};
